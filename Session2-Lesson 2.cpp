da46973e3074eb7cc68e436422845392088f0274
Hoang2006-creater <duhoang1611@gmail.com>
2024-11-20 21:25:15 +0700

    Add files via upload

---
+++ Session2-Lesson 2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+    int main() {
+    	// Khai bao va khoi tao hai bien so nguyen 
+    	int firstnumber = 16;
+    	int secondnumber = 11;
+    	// Tinh tong cua hai so nguyen va luu vao bien khac 
+    	int sum = firstnumber + secondnumber;
+    	//In  ket qua ra man hinh 
+    	printf("Tong cua %d va %d la: %d\n", firstnumber, secondnumber, sum);
+    	
+    	return 0;
+}
