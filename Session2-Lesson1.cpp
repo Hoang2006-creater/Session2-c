81f4ffec054a1e6d046a4265a1bac52de7e79bab
Hoang2006-creater <duhoang1611@gmail.com>
2024-11-20 21:48:12 +0700

    Add files via upload

---
+++ Session2-Lesson1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int x = 10;//Luu tru so nguyen x bang 10
+	float pi = 3.14;// Luu tru so thuc pi = 3.14 
+	char grade = 'B';// Luu tru mot ki tu duy nhat 
+	printf("So nguyen: %d\n", x);  
+    printf("So thuc float: %.2f\n", pi); 
+    printf("Ky tu: %c\n", grade); 
+    
+    return 0; 
+}
